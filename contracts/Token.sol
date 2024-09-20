75f7c8b352447d5b8221ba4654b6a0db61368368
nancygantinoroblake5 <nancygantinoroblake5kr@hotmail.com>
2024-09-20 06:30:52 +0000

    Initialised

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("0x288b3daebd813405babba9e2f03154b8674731a11bc2472795a884621a04109b","0x288b3daebd813405babba9e2f03154b8674731a11bc2472795a884621a04109b"){} 
+    constructor()ERC20("288b3daebd813405babba9e2f03154b8674731a11bc2472795a884621a04109b","288b3daebd813405babba9e2f03154b8674731a11bc2472795a884621a04109b"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
